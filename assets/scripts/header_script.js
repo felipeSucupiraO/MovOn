13d8092d5336fba28555254bb03fb576a21047ba
Felipe <felipeuberaba10@gmail.com>
2023-08-07 18:21:33 -0300

    Optimization on the profile card code

--- assets/scripts/header_script.js
+++ assets/scripts/header_script.js
@@ -2,9 +2,5 @@ var profileCard = $(".profile-card");
 var headerProfileIcon = $(".profile-section > .profile-icon");
 
 headerProfileIcon.on("click", function() {
-    if (profileCard.css("display") == "none") {
-        profileCard.css("display", "grid");
-    } else {
-        profileCard.css("display", "none");
-    }
+    profileCard.toggle();
 });
