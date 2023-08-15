96126eca72f1f643c056087f5d7dfb537a236891
Aust2000 <felipeuberaba10@gmail.com>
2023-08-15 10:55:00 -0300

    my list remove added

--- assets/scripts/my_list_creation.js
+++ assets/scripts/my_list_creation.js
@@ -1,29 +1,49 @@
 var movieList = $(".my-list-list ul");
 var movieCard = $(".my-list-list li");
+var removeButtons;
 
 movieCard.detach();
 
 var data;
 fetch("../data/movies.json")
-    .then((request) => {
-        return request.json();
-    })
-    .then((data) => {
-        data.movies.forEach(i => {
-            if (sessionStorage.getItem(i.name) != null) {
-                movieCard.clone().appendTo(movieList);
+.then((request) => {
+    return request.json();
+})
+.then((data) => {
+    data.movies.forEach(i => {
+        if (sessionStorage.getItem(i.name) != null) {
+            movieCard.clone().appendTo(movieList);
+            
+            let current = $(".my-list-list li:last-child");
+            
+            makeMovieCard(current, i);
+        }
+    });
+})
+.then(() => {
+    removeButtons = $(".my-list-button");
+    removeButtons.on("click", removeMovie);
+})
 
-                let current = $(".my-list-list li:last-child");
-
-                makeMovieCard(current, i);
-            }
-        });
-    })  
-    
 function makeMovieCard(movieCard, movieData) {
-    movieCard.find(".my-list-card").addClass(movieData.name);
+    movieCard.find(".movie-card").addClass(movieData.name);
     movieCard.find(".movie-vertical-poster").attr("src", movieData.verticalImageUrl);
     movieCard.find(".movie-vertical-poster").attr("alt", movieData.imageAlt);
     movieCard.find(".play-button").attr("href", movieData.moviePageUrl);
     movieCard.find(".play-button").attr("title", movieData.moviePageUrlTitle);
+}
+
+
+function removeMovie() {
+    // the following line gets the card that was clicked (the div element)
+    let card = $(event.target).parents(".movie-card");
+    let classList = getClassList(card[0])
+    let movie = classList[classList.length - 1];
+    sessionStorage.removeItem(movie);
+    
+    location.reload();
+}
+
+function getClassList(element) {
+    return element.className.split(/\s+/);
 }
