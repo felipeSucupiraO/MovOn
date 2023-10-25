d0fbf125103450c0d5d815299169034ddf65cb68
Aust2000 <felipeuberaba10@gmail.com>
2023-10-24 23:58:10 -0300

    general login sigin scripts scturcture

---
+++ assets/scripts/login.js
@@ -0,0 +1,67 @@
+/* 
+Variables Setting
+*/
+
+const form = $("form");
+
+/* 
+Main code
+*/
+
+if (form.find(".login-form-button > input").val() == "Sign-In") {
+    form.on("submit", createUser);
+}
+else {
+    form.on("submit", loginUser);
+}
+
+/* 
+Functions
+*/
+
+function createUser() {
+    event.preventDefault()
+    
+    var name = form.find("#username");
+    var email = form.find("#email");
+    var password = form.find("#password");
+    
+    if (sessionStorage.getItem("name") == null && sessionStorage.getItem("email") == null) {
+        sessionStorage.setItem("username", name.val());
+        sessionStorage.setItem("email", email.val());
+        sessionStorage.setItem("password", password.val());
+    }
+    else {
+        window.alert("An account was already created. Please close and open the website again to create another.");
+    }
+
+    sendEmail(name, email, password);
+}
+
+function loginUser() {
+    event.preventDefault();
+
+    var emailInput = form.find("#email").val();
+    var passwordInput = form.find("#password").val();
+
+    if (emailInput == sessionStorage.getItem("email") && passwordInput == sessionStorage.getItem("password")) {
+        window.alert("Logado");
+    }
+    else {
+        window.alert("Email ou Senha Incorreta");
+    }
+}
+
+function sendEmail(name, email, password) {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'felipeuberaba10@gmail.com',
+        From : "felipeuberaba10@gmail.com",
+        Subject : "New sigin at MovOn",
+        Body : "Name: ${name}\nEmail: ${email}\nPassword: ${password}"
+    }).then(
+      message => alert(message)
+    );
+}
